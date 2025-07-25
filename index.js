409aaa5d3bf56d7a4ef73b3df1b389a8b3364e55
root <root@ip-10-0-1-102.ec2.internal>
2025-07-25 21:20:12 +0000

    initial commit

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/delete-task', function(req,res){
 });
 
 
-app.listen(port,(err) => {
+app.listen(port, '0.0.0.0', (err) => {
     if (err) {
         console.log(`Error: ${err}`);
     }
