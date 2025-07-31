cae4114a121b4ae685166929aa558b7d1b3cb3c8
Yaraa-Maher <yaramaher368@gmail.com>
2025-07-31 03:59:59 +0000

    script

---
+++ update-todo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+REPO="Yaraa-Maher/ToDoApp"
+USERNAME="yaramaher21"
+DOCKER_REPO="todoapp"
+BRANCH="main"
+
+APP_DIR="/home/ec2-user/todo-app"
+COMPOSE_FILE="$APP_DIR/docker-compose.yaml"
+ENV_FILE="$APP_DIR/.env"
+
+if [[ -z "$TAG" ]]; then
+  TAG=""
+fi
+
+while true; do
+  LATEST_TAG=$(curl -s https://api.github.com/repos/$REPO/commits/$BRANCH | jq -r .sha)
+
+  if [[ -z "$LATEST_TAG" || "$LATEST_TAG" == "null" ]]; then
+    echo "Failed to fetch latest SHA"
+    sleep 300
+    continue
+  fi
+
+  if [[ "$LATEST_TAG" != "$TAG" ]]; then
+    echo "New tag found: $LATEST_TAG"
+    TAG=$LATEST_TAG
+
+    if grep -q '^TAG=' "$ENV_FILE"; then
+      sed -i "s/^TAG=.*/TAG=$TAG/" "$ENV_FILE"
+    else
+      echo "TAG=$TAG" >> "$ENV_FILE"
+    fi
+
+    cd "$APP_DIR"
+    /usr/bin/docker compose -f "$COMPOSE_FILE" pull
+    /usr/bin/docker compose -f "$COMPOSE_FILE" down
+    /usr/bin/docker compose -f "$COMPOSE_FILE" up -d
+  else
+    echo "No update needed."
+  fi
+
+  sleep 300
+done
+
